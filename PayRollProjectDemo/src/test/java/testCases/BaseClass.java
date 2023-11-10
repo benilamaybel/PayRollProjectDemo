027d102cac693217fb9f1e1b02c4228dde33385a
USER <USER@DESKTOP-56HVALE>
2023-11-10 13:56:29 +0530

    remove comment

--- PayRollProjectDemo/src/test/java/testCases/BaseClass.java
+++ PayRollProjectDemo/src/test/java/testCases/BaseClass.java
@@ -73,7 +73,7 @@ public class BaseClass {
 	@AfterMethod(alwaysRun = true) // to run always
 	public void afterMethod(ITestResult iTestResult) throws IOException { // ITestResult is a Listener class
 		if (iTestResult.getStatus() == ITestResult.FAILURE) { // executes only when TC Fails
-			System.out.println("Testcase Failed , caling screenshot function!");
+			System.out.println("Testcase Failed , calling screenshot function!");
 			sc = new ScreenShotCapture(); // Object creation for ScreenShotCapture class
 			sc.captureFailureScreenShot(driver, iTestResult.getName()); // calls captureFailureScreenShot method; passes
 																		// the name of Testcase
